1640862e78af740d6d76d4c99a62fe72e80a29f6
GlebKolyano <glebkolyano38@gmail.com>
2021-12-04 15:43:50 +0300

    Styles: add styles and html for main__wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,18 @@ import "./styles/app.css"
 
 
 function App() {
-  useEffect(() => {
-    fetchBooks()
-  }, [])
-  async function fetchBooks() {
-    const responce = await axios.get('https://www.googleapis.com/books/v1/volumes?q=isbn:0771595158')
-    console.log(responce.data)
-  }
+  
+  
   return (
     <div className="app-wrapper">
-      <header className="header"><a href="/">Shopping cart project</a></header>
-      <main className="main">main block</main>
+      <header className="header"><a href="/">Books for everyone</a></header>
+      <main className="main">
+        <div className="main__wrapper">
+          <div className="main__content">content</div>
+          <div className="main__cart">cart</div>
+        </div>
+
+      </main>
       <footer className="footer">footer</footer>
     </div>
   );
