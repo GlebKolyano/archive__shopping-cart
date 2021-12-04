ec3a5ad9eb140606029eee3adf180d9ec5ba8b2a
GlebKolyano <glebkolyano38@gmail.com>
2021-12-04 13:42:05 +0300

    Make the main site structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import "./styles/app.css"
 
 
 function App() {
   return (
-    <div className="App">
-   
+    <div className="app-wrapper">
+      <header className="header">Shopping cart project</header>
+      <main className="main">main block</main>
+      <footer className="footer">footer</footer>
     </div>
   );
 }
