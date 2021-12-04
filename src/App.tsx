8b423ac107100a75eb5c01c6f953b50782ceb512
GlebKolyano <glebkolyano38@gmail.com>
2021-12-04 13:54:05 +0300

    Styles: add css for the main blocks

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./styles/app.css"
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="header">Shopping cart project</header>
+      <header className="header"><a href="/">Shopping cart project</a></header>
       <main className="main">main block</main>
       <footer className="footer">footer</footer>
     </div>
