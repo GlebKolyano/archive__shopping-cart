070dcb565cdb1a3bed02b94b7f013539d295ac6f
GlebKolyano <glebkolyano38@gmail.com>
2021-12-04 19:04:10 +0300

    Create BookList.jsx component and add css styles for its

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -5,14 +5,18 @@ const BookList = () => {
   return (
       <ul className="books__wrapper">{products.map(book => 
         <li key={book._id} className="book__item"><a href={"#"+ book._id}>
-            <div>{book.title}</div>
+            
             <div>
-                <img className="book__cover" src={book.image} alt={book.title}></img>
+                <img className="book__cover" src={book.image} alt={book.title}/>
             </div>
-            <div className="book__price">
-              <div>{book.price}</div>
-              <button>Add To Cart</button>
+            <div className="book__info">
+              <h2 className="book__title">{book.title}</h2>
+              <div div className="book__price-wrapper">
+              <div className="book__price" >{book.price} р.</div>
+              <button className="book__button button">В корзину</button>
             </div>
+            </div>
+
         </a></li>  )}
       </ul>
   )
