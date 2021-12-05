1e733b4a4821249f1b462264f4ced830cb1001ec
GlebKolyano <glebkolyano38@gmail.com>
2021-12-05 16:16:11 +0300

    Add transitiong group/animation for BookList.jsx component

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -1,14 +1,27 @@
 import { useEffect, useState } from "react"
 import data from "../data.json"
 import BookItem from "./BookItem"
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
 
 const BookList = ({books}) => {
 
   
   return (
       <ul className="books__wrapper">
-        {books.map(book => <BookItem key={book._id} book={book}/>
+        <TransitionGroup component={null}>
+        {books.map(book => 
+          <CSSTransition
+              key={book._id}
+              timeout={500}
+              classNames="book"
+            >
+            <BookItem  book={book}/>
+          </CSSTransition>
          )}
+        </TransitionGroup>
       </ul>
   )
 }
